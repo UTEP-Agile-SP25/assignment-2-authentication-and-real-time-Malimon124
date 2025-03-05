8049d2212596048eabe23b91da003d37611d68de
Matthew Limon <malimon2@miners.utep.edu>
2025-03-05 11:21:34 -0700

    src and dist folders added to repo. config.js added with code for firebase.

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxZ3HmSEwe7abkV2X-uE83pO6OlGC1Jys",
+  authDomain: "limon-sandbox.firebaseapp.com",
+  projectId: "limon-sandbox",
+  storageBucket: "limon-sandbox.firebasestorage.app",
+  messagingSenderId: "256984171237",
+  appId: "1:256984171237:web:31613d9ae8c0bcc1baca0c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
